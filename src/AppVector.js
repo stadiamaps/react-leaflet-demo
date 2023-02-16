b5859fc16c3735d788e1d6d11a5d1fe8d78672e7
Ian Wagner <ian.wagner@stadiamaps.com>
2023-02-16 23:49:10 +0900

    Add gameboy style

--- src/AppVector.js
+++ src/AppVector.js
@@ -27,7 +27,7 @@ function AppVector() {
         scrollWheelZoom={true}>
         <MapLibreTileLayer
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
-          url="https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json"
+          url={require('./gameboy.json')}
         />
         <MarkerClusterGroup>
           {arcades.features.map((arcade, index) => (
